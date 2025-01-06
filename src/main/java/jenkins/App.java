cd6410b119f03739bbfaddcfabca0f6fc213c382
root <root@ip-172-31-36-216.ap-south-1.compute.internal>
2025-01-06 07:58:24 +0000

    hello jenkins

--- src/main/java/jenkins/App.java
+++ src/main/java/jenkins/App.java
@@ -5,6 +5,6 @@ package jenkins;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello jenkins console!!!!!");
     }
 }
